bf72c43d8bcccb1e4ad070dd724c5d291f158ca4
Carlos Ramos <skhcri@gmail.com>
2021-10-15 10:36:55 +0200

    minor bugfixes

--- utils/utils.php
+++ utils/utils.php
@@ -3,6 +3,8 @@
     function isActiveMenu(string $option) : bool {
         if (strpos($_SERVER["REQUEST_URI"],$option)) {
             return true;            
+        } elseif (strpos($_SERVER["REQUEST_URI"],"/")){
+            return true;
         } else {
             return false;
         }
