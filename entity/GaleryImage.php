5e4c87e37e25cbb8399b3b630fdf058f6f21d9bb
Carlos Ramos <skhcri@gmail.com>
2021-10-18 09:39:24 +0200

    GaleryImages commit

---
+++ entity/GaleryImage.php
@@ -0,0 +1,137 @@
+<?php
+
+class GaleryImage {
+    /**
+     * @var [string]
+     */
+    private $name;
+    /**
+     * @var [string]
+     */
+    private $description;
+    /**
+     * @var [int]
+     */
+    private $numVisualizations;
+    /**
+     * @var [int]
+     */
+    private $numLikes;
+    /**
+     * @var [int]
+     */
+    private $numDownloads;
+
+    public function __construct(string $name, 
+                                string $description,
+                                int $numVisualizations = 0,
+                                int $numLikes = 0,
+                                int $numDownloads = 0)
+    {
+        $this->name = $name;
+        $this->description = $description;
+        $this->numVisualizations = $numVisualizations;
+        $this->numLikes = $numLikes;
+        $this->numDownloads = $numDownloads;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+    /**
+     * Get the value of description
+     */ 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */ 
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+    /**
+     * Get the value of numVisualizations
+     */ 
+    public function getNumVisualizations()
+    {
+        return $this->numVisualizations;
+    }
+
+    /**
+     * Set the value of numVisualizations
+     *
+     * @return  self
+     */ 
+    public function setNumVisualizations($numVisualizations)
+    {
+        $this->numVisualizations = $numVisualizations;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of numLikes
+     */ 
+    public function getNumLikes()
+    {
+        return $this->numLikes;
+    }
+
+    /**
+     * Set the value of numLikes
+     *
+     * @return  self
+     */ 
+    public function setNumLikes($numLikes)
+    {
+        $this->numLikes = $numLikes;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of numDownloads
+     */ 
+    public function getNumDownloads()
+    {
+        return $this->numDownloads;
+    }
+
+    /**
+     * Set the value of numDownloads
+     *
+     * @return  self
+     */ 
+    public function setNumDownloads($numDownloads)
+    {
+        $this->numDownloads = $numDownloads;
+
+        return $this;
+    }
+}
+
+?>
