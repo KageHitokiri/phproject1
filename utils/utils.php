4bd33d36f1512fd9a84587ac0880cd4d7632aa3d
Carlos Ramos <skhcri@gmail.com>
2021-10-18 09:25:29 +0200

    minor bugfixes

--- utils/utils.php
+++ utils/utils.php
@@ -1,12 +1,20 @@
 <?php
-
+    
     function isActiveMenu(string $option) : bool {
-        if (strpos($_SERVER["REQUEST_URI"],$option)) {
-            return true;            
-        } elseif (strpos($_SERVER["REQUEST_URI"],"/")){
-            return true;
+        if  (strpos($_SERVER["REQUEST_URI"],$option) ) {
+            return true;        
         } else {
             return false;
         }
+    }
+
+    function existsInActiveArrayMenu (array $options): bool {
+        foreach ($options as $value) {
+            if (isActiveMenu($value)) {
+                return true;
+            } else {
+                return false;
+            }
+        }        
     }
 ?>
--- views/partials/nav-doc.part.php
+++ views/partials/nav-doc.part.php
@@ -15,14 +15,17 @@
        <div class="collapse navbar-collapse navbar-right" id="menu">
           <ul class="nav navbar-nav">
             <li class="<?=(isActiveMenu("index")? "active":"");?> lien">
-                <a href="<?=(isActiveMenu("index")? "#":"/");?>">
+                <a href="<?=(isActiveMenu("index")? "#":"/index.php");?>">
                 <i class="fa fa-home sr-icons"></i> Home</a></li>
+
             <li class="<?=(isActiveMenu("about")? "active":"");?> lien">
                 <a href="<?=(isActiveMenu("about")? "#":"/about.php");?>">
                 <i class="fa fa-bookmark sr-icons"></i> About</a></li>
-            <li class="<?=(isActiveMenu("blog")? "active":"");?> lien">
+
+            <li class="<?=(existsInActiveArrayMenu(["blog","single_post"])? "active":"");?> lien">
                 <a href="<?=(isActiveMenu("blog")? "#":"/blog.php");?>">
                 <i class="fa fa-file-text sr-icons"></i> Blog</a></li>
+                
             <li class="<?=(isActiveMenu("contact")? "active":"");?> lien">            
                 <a href="<?=(isActiveMenu("contact")? "#":"/contact.php");?>">
                 <i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
