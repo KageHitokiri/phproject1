f8fb62c95c8044a8d389d496591ab8b7161e4206
Carlos Ramos <skhcri@gmail.com>
2021-10-20 08:39:57 +0200

    Creada galería en index.php

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
     $title = "Home";
     require_once "./utils/utils.php";
-    include("./views/index.view.php");
+    require_once "./entity/ImageGallery.php";
+    include("./views/index.view.php");    
+    
+    $gallery[] = new ImageGallery("1.jpg","Sin descripción",1,2,10);
+    $gallery[] = new ImageGallery("2.jpg","Sin descripción",1,2,10);
+    $gallery[] = new ImageGallery("3.jpg","Sin descripción",1,2,10);
+    $gallery[] = new ImageGallery("4.jpg","Sin descripción",1,2,10);
+    $gallery[] = new ImageGallery("5.jpg","Sin descripción",1,2,10);
+    $gallery[] = new ImageGallery("6.jpg","Sin descripción",1,2,10);
+    $gallery[] = new ImageGallery("7.jpg","Sin descripción",1,2,10);
+    $gallery[] = new ImageGallery("8.jpg","Sin descripción",1,2,10);
+    $gallery[] = new ImageGallery("9.jpg","Sin descripción",1,2,10);
+    $gallery[] = new ImageGallery("10.jpg","Sin descripción",1,2,10);
+    $gallery[] = new ImageGallery("11.jpg","Sin descripción",1,2,10);
+    $gallery[] = new ImageGallery("12.jpg","Sin descripción",1,2,10);
+    
+    
 
     
