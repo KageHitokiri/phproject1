3adec15cf5f40f4c65a6905ca4313938d43365fc
Carlos Ramos <skhcri@gmail.com>
2021-10-22 10:37:45 +0200

    comprobaciones: Descripción, tamaño y procesado

--- gallery.php
+++ gallery.php
@@ -3,6 +3,33 @@
     $title = "Gallery";
     require_once "./utils/utils.php";
 
+    $info = $description = $imgUrl = "";
+    $descriptionError = $imageError = $thereAreErrors = false;
+    $errors = [];
+
+    if ("POST" === $_SERVER["REQUEST_METHOD"]) {
+        if (empty($_POST)) {
+            $errors[] = "Se ha producido un error al procesar el formulario";
+            $imageError = true;            
+        }
+
+        if (empty($description)) {
+            $errors[] = "La descripción es obligatoria";
+            $descriptionError = true;
+        }
+
+        if (isset($_FILES['image']) && ($_FILES['image']['error'] == UPLOAD_ERR_OK)) {
+            if($_FILES['image']['size'] > (2 * (1024**2))) {
+                $errors[] = "Este archivo es demasiado pesado, los archivos no pueden pesar más de 2MB";
+                $imageError = true;
+            }
+        }
+
+        
+    }
+
+    
+
     include("./views/gallery.view.php");
 
 ?>
