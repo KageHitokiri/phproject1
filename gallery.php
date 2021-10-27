e3b09464ae068dc8a619a0f024b4d5fb80a63aa2
Carlos Ramos <skhcri@gmail.com>
2021-10-27 09:09:59 +0200

    Implemented: Resize

--- gallery.php
+++ gallery.php
@@ -5,6 +5,7 @@
     require_once "./entity/ImageGallery.php";
     require_once "./utils/File.php";
     require_once "./exceptions/FileException.php";
+    require_once "./utils/SimpleImage.php";
 
     $info = $description = $imgUrl = "";
     $descriptionError = $imageError = $thereAreErrors = false;
@@ -19,6 +20,20 @@
                                        array('image/jpeg','image/jpg','image.png'),
                                        (2*(1024**2)));
             $imageFile->saveUploadedFile(ImageGallery::PATH_IMAGE_GALLERY);
+            try {
+                $simpleImage = new \claviska\SimpleImage();
+                $simpleImage
+                ->fromFile(ImageGallery::PATH_IMAGE_GALLERY . $imageFile->getFileName())
+                ->resize(975, 525)
+                ->toFile(ImageGallery::PATH_IMAGE_PORTFOLIO . $imageFile->getFileName())
+                ->resize(650, 350)
+                ->toFile(ImageGallery::PATH_IMAGE_GALLERY . $imageFile->getFileName());                
+            } catch (Exception $err) {
+                $errors[] = $err->getMessage();
+                $imageError = true;
+            }
+        
+
         } catch (FileException $fe) {
             $errors[] = $fe->getMessage();
             $imageError = true;
